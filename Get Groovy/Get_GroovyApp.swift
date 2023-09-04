f486689c6cc3505d5e47d11fe5ed09b1a43ae825
Tyler <kamholz.tyler@gmail.com>
2023-09-03 21:35:49 -0500

    Initial Commit

---
+++ Get Groovy/Get_GroovyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Get_GroovyApp.swift
+//  Get Groovy
+//
+//  Created by Tyler  Kamholz on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Get_GroovyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
